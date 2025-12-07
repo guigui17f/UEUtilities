c038d2367b342d36b13294c9b616f029047cfa36
guigui17f <guigui17f0@gmail.com>
2025-12-07 14:47:05 +0800

    upgrade to UE 5.7

--- Plugins/WebPSupport/Source/WebPSupport/WebPSupport.Build.cs
+++ Plugins/WebPSupport/Source/WebPSupport/WebPSupport.Build.cs
@@ -6,7 +6,7 @@ public class WebPSupport : ModuleRules
 {
 	public WebPSupport(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 		
 		PublicIncludePaths.AddRange(
 			new string[] {
